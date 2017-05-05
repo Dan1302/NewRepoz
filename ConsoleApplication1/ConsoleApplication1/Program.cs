adb3472ee9bae7d5b2f4040546cf45d9e3c5b6e3
Daniielian Diana <dianadaniyelyan1302@gmail.com>
2017-05-05 07:47:03 +0300

    Изменения

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            {
+                Console.WriteLine("Как тебя зовут?");
+                name = Console.ReadLine();
+            }
+            int age;
+            {
+                Console.WriteLine("Cколько тебе лет?");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            int x;
+            x = 2009 - age;
+            Console.WriteLine(name + " родился в " + x + " году");
+            x = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
